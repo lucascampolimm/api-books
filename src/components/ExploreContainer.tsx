0e27f90e87740d2f88acd9708acdc84a6bbc7395
Lucas Campolim <lucascampolim@protonmail.com>
2023-09-19 04:53:58 -0300

    format

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,33 +1,42 @@
-import { AppConfig } from './config';
-import './ExploreContainer.css';
-import axios from 'axios';
+import { AppConfig } from "./config";
+import "./ExploreContainer.css";
+import axios from "axios";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        Start with Ionic{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
     </div>
   );
 };
 
 const API_KEY = AppConfig.apiKey;
-const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
+const BASE_URL = "https://www.googleapis.com/books/v1/volumes";
 
-const searchBooks = async (query:any) => {
+const searchBooks = async (query: any) => {
   try {
     const response = await axios.get(`${BASE_URL}?q=${query}&key=${API_KEY}`);
     return response.data;
   } catch (error) {
-    console.error('Erro ao buscar livros:', error);
+    console.error("Erro ao buscar livros:", error);
     throw error;
   }
 };
 
 // Uso estático
-const result = await searchBooks('React Native');
+const result = await searchBooks("React Native");
 console.log(result);
 
 export default ExploreContainer;
